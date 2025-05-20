089af7996db0781e6529c0c5934a5337feb132b3
Maximuser505505 <csfeliciano@msi.upd.edu.ph>
2025-05-20 14:37:33 +0800

    Update coastlines.py

--- dfm_tools/coastlines.py
+++ dfm_tools/coastlines.py
@@ -22,63 +22,23 @@ def bbox_convert_crs(bbox, crs):
     return bbox
 
 
-def get_coastlines_gdb(res:str='h', bbox:tuple = (-180, -90, 180, 90), min_area:float = 0, crs:str = None, columns:list = ['area']) -> gpd.geoseries.GeoSeries:
-    """
-    GSHHS coastlines: https://www.ngdc.noaa.gov/mgg/shorelines/data/gshhg/latest/readme.txt
-    geopandas docs https://geopandas.org/en/stable/docs/reference/api/geopandas.read_file.html
+def get_coastlines_mb(bbox:tuple = (-180, -90, 180, 90), min_area:float = 0, crs:str = None, columns:list = ['area']) -> gpd.geoseries.GeoSeries:
     
-    Parameters
-    ----------
-    res : str, optional
-        f(ull), h(igh), i(ntermediate), l(ow), c(oarse) resolution. The default is 'h'.
-    bbox : tuple, optional
-        (minx, miny, maxx, maxy), also includes shapes that are partly in the bbox. The default is (-180, -90, 180, 90).
-    min_area : float, optional
-        in km2, min_area>0 speeds up process. The default is 0.
-    crs : str, optional
-        coordinate reference system
-    columns : list, optional
-        which shapefile columns to include, None gives all. The default is ['area'].
-
-    Returns
-    -------
-    coastlines_gdb : TYPE
-        DESCRIPTION.
-
-    """
-    
-    if res not in 'fhilc':
-        raise KeyError(f'invalid res="{res}", resolution options are f(ull), h(igh), i(ntermediate), l(ow), c(oarse)')
     if crs is not None:
         bbox = bbox_convert_crs(bbox, crs)
         
-    # download gshhs data if not present and return dir
-    dir_gshhs = gshhs_coastlines_shp()
+    main_dir = 'C:/Users/csfel/Desktop/Delft3D-Projects/Input Data'
+    dir_coastlines_mb = os.path.join(main_dir, f'Coastline_Scenario_B_Modified.shp')
+    coastlines_gdb_mb = gpd.read_file(dir_coastlines_mb, columns=columns, bbox=bbox)
+    coastlines_gdb_list = [coastlines_gdb_mb]
 
-    file_shp_L1 = os.path.join(dir_gshhs,'GSHHS_shp',res,f'GSHHS_{res}_L1.shp') #coastlines
-    file_shp_L6 = os.path.join(dir_gshhs,'GSHHS_shp',res,f'GSHHS_{res}_L6.shp') #Antarctic grounding-line polygons
-    file_shp_L2 = os.path.join(dir_gshhs,'GSHHS_shp',res,f'GSHHS_{res}_L2.shp') #lakes
-    file_shp_L3 = os.path.join(dir_gshhs,'GSHHS_shp',res,f'GSHHS_{res}_L3.shp') #islands-in-lakes
-    
-    print('>> reading coastlines: ',end='')
-    dtstart = dt.datetime.now()
-    coastlines_gdb_L1 = gpd.read_file(file_shp_L1, columns=columns, where=f"area>{min_area}", bbox=bbox)
-    coastlines_gdb_L6 = gpd.read_file(file_shp_L6, columns=columns, where=f"area>{min_area}", bbox=bbox)
-    coastlines_gdb_list = [coastlines_gdb_L1,coastlines_gdb_L6]
-    if len(coastlines_gdb_L1)<2: #if only one L1 polygon is selected, automatically add lakes and islands-in-lakes
-        coastlines_gdb_L2 = gpd.read_file(file_shp_L2, columns=columns, where=f"area>{min_area}", bbox=bbox)
-        coastlines_gdb_list.append(coastlines_gdb_L2)
-        coastlines_gdb_L3 = gpd.read_file(file_shp_L3, columns=columns, where=f"area>{min_area}", bbox=bbox)
-        coastlines_gdb_list.append(coastlines_gdb_L3)
-    
     # remove empty geodataframes from list to avoid FutureWarning
     # escape for empty resulting list and concatenate otherwise
     coastlines_gdb_list = [x for x in coastlines_gdb_list if not x.empty]
     if not coastlines_gdb_list:
         return gpd.GeoDataFrame()
     coastlines_gdb = pd.concat(coastlines_gdb_list)
-    print(f'{(dt.datetime.now()-dtstart).total_seconds():.2f} sec')
-    
+
     if crs:
         coastlines_gdb = coastlines_gdb.to_crs(crs)
     
@@ -127,31 +87,29 @@ def get_borders_gdb(res:str='h', bbox:tuple = (-180, -90, 180, 90), crs:str = No
     return coastlines_gdb
 
 
-def plot_coastlines(ax=None, res:str='h', min_area:float = 0, crs=None, **kwargs):
+def plot_coastlines_mb(ax=None, min_area:float = 0, crs=None, **kwargs):
     """
     get coastlines with get_coastlines_gdb and bbox depending on axlims, plot on ax and set axlims back to original values
     """
-    #TODO: if ax is GeoAxis, get crs from ax
-    
     if ax is None:
         ax = plt.gca()
-    
+
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
     bbox = (xlim[0], ylim[0], xlim[1], ylim[1])
-    
+
     if 'edgecolor' not in kwargs:
         kwargs['edgecolor'] = 'k'
     if 'facecolor' not in kwargs:
         kwargs['facecolor'] = 'none'
     if 'linewidth' not in kwargs:
         kwargs['linewidth'] = 0.5
-    
-    coastlines_gdb = get_coastlines_gdb(bbox=bbox, res=res, min_area=min_area, crs=crs)
-    if coastlines_gdb.empty:
+
+    coastlines_mb = get_coastlines_mb(bbox=bbox, min_area=min_area, crs=crs)
+    if coastlines_mb.empty:
         return
     
-    coastlines_gdb.plot(ax=ax, **kwargs)
+    coastlines_mb.plot(ax=ax, **kwargs)
     
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
